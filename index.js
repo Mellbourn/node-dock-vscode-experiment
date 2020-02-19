b2f4def58e5277e2ff5b48a5f355734831530b4a
Klas Mellbourn <klas@mellbourn.net>
2020-02-19 23:00:19 +0100

    node server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("hello world"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
